814e7306e721e70fa5221d13d8a886ba455a7a91
Mike Stok <mike@stok.ca>
2011-04-17 20:26:15 -0400

    Initial commit of a "fresh" Rails project.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SuzanneLasdb::Application.config.secret_token = 'c81457e051ac1610dc4b6e5051cb4118f00cdbf58c2888142a28c14765da380b77ccd0c548a12f1baa8f852742fdd256296d5272920853b6d36619670d56a2df'
