484ff15f31a9c1b6e393449e128edc01ae1def73
Mike Stok <mike@stok.ca>
2011-04-17 21:08:39 -0400

    Ran: rake db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110418010047) do
+ActiveRecord::Schema.define(:version => 20110418010634) do
+
+  create_table "answers", :force => true do |t|
+    t.integer  "question_id"
+    t.text     "text"
+    t.boolean  "correct?"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "questions", :force => true do |t|
     t.text     "prompt"
