e1200169425d7716c65c1c8ba863cb19b5a36b34
Mike Stok <mike@stok.ca>
2011-04-20 04:37:25 -0400

    Adds category to question

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110418105945) do
+ActiveRecord::Schema.define(:version => 20110420082809) do
 
   create_table "answers", :force => true do |t|
     t.integer  "question_id"
@@ -24,6 +24,7 @@ ActiveRecord::Schema.define(:version => 20110418105945) do
     t.text     "prompt"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "category"
   end
 
 end
