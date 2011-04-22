4ddda6ffb22f0e0dd9568e400296fc6cedc500ba
Mike Stok <mike@stok.ca>
2011-04-22 08:08:39 -0400

    Fixes tests so the run again.
    No real content :-(

--- test/unit/question_test.rb
+++ test/unit/question_test.rb
@@ -1,7 +1,27 @@
 require 'test_helper'
 
 class QuestionTest < ActiveSupport::TestCase
+  def setup
+    @valid_q_params = {
+      :prompt => 'This is a valid question because:',
+      :answers_attributes => [
+        {
+          :text => 'It has a prompt',
+          :correct => false,
+        },
+        {
+          :text => 'It has an answer',
+          :correct => false,
+        },
+        {
+          :text => 'Both of the above',
+          :correct => true,
+        }
+      ],
+    }
+  end
+
   def test_should_be_valid
-    assert Question.new.valid?
+    assert Question.new(@valid_q_params).valid?
   end
 end
