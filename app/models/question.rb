c0b8ae1ac9535024e63a91d432c1019f6604c03f
Mike Stok <mike@stok.ca>
2012-01-04 23:30:50 -0500

    gi-10 move RefResolver into its own file.

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,30 +1,4 @@
 class Question < ActiveRecord::Base
-  # This class encapuslates the conversion of {{ref}} in a piece of
-  # text to the appropriate tag.
-  class RefResolver
-    # in: list of references
-    #     list of tags
-    #
-    # sets up state so we can later map any non-blank 
-    # references to the associated tag.
-    def initialize(ref_list, tag_list)
-      @ref_to_tag =
-        ref_list.zip(tag_list).
-        reject {|pair| pair[0].blank? }.
-        inject({}) do |hash, pair|
-          hash.merge({ pair[0] => pair[1] })
-        end
-    end
-
-    def resolve_refs(text)
-      result = text.dup
-      @ref_to_tag.each_pair do |ref, tag|
-        result.gsub!(/{{#{ref}}}/, tag)
-      end
-      return result
-    end
-  end
-
   attr_accessible :prompt,
     :category_id,
     :answers, :answers_attributes
@@ -34,6 +8,8 @@ class Question < ActiveRecord::Base
     :reject_if => lambda { |a| a[:text].blank? },
     :allow_destroy => true
   validates_presence_of :prompt, :answers
+
+  require 'ref_resolver'
 
   # Finds the most recent updated_at for the question and
   # any of its answers.
