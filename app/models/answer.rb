764228c71d9fe9043bd6e4bffa46ee97eaa0b2ec
Mike Stok <mike@stok.ca>
2012-01-04 22:07:46 -0500

    gi-10 - performs reference substitution on answers
    now {{ref}} in an answer gets turned into the right tag for display.

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,3 +1,9 @@
 class Answer < ActiveRecord::Base
   belongs_to :question
+  validates_format_of :ref,
+    :with => /\A[A-Za-z0-9]{1,5}\Z/,
+    :allow_blank => true
+  validates_uniqueness_of :ref,
+    :scope => :question_id,
+    :allow_blank => true
 end
--- app/models/question.rb
+++ app/models/question.rb
@@ -31,4 +31,59 @@ class Question < ActiveRecord::Base
     else category.full_name
     end
   end
+
+  require "ostruct"
+
+  # in:
+  # out:
+  def answer_list
+    tags = tag_list
+    tag_for = ref_to_tag(tags)
+
+    answers.each.collect do |a|
+      text = translate_refs(a.text, tag_for)
+
+      OpenStruct.new({
+        :tag     => tags.shift,
+        :text    => text,
+        :correct => a.correct,
+      })
+    end
+  end
+
+  private
+
+  # in: nothing
+  # out: list ot tags the right length for this questions answers
+  def tag_list
+    tags = []
+    tag = 'a'
+    answers.size.times do
+      tags << tag
+      tag = tag.succ
+    end
+    return tags
+  end
+
+  # in: tag list
+  # out: hash mapping question's refs to tag names
+  def ref_to_tag(tag_list)
+    map = {}
+    answers.each_with_index do |a, i|
+      if a.ref
+        map[a.ref] = tag_list[i]
+      end
+    end
+
+    return map
+  end
+
+  def translate_refs(text, map)
+    result = text.dup
+    map.each_pair do |ref, tag|
+      result.gsub!(/{{#{ref}}}/, tag)
+    end
+
+    return result
+  end
 end
