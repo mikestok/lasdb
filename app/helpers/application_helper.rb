76b153bd450ef418b0fd194c726119cc6bcca15e
Mike Stok <mike@stok.ca>
2012-01-08 11:59:06 -0500

    gi-15 Adds icons to the edit question page.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,14 @@
 module ApplicationHelper
+  # Make a link to an icon.  In the future the colour, size, and format
+  # might come from application configuration.
+  def make_icon_tag(image_name, opts={})
+    colour = "blue"
+    size   = "16x16"
+    format = "png"
+    file   =
+      ["icons", format, colour, size, "#{image_name}.#{format}"].join('/')
+
+    opts[:size] ||= size
+    image_tag file, opts          
+  end
 end
